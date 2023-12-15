81e2d87792b95e58a1b4a4aff3ec6af0b6482f40
Jason H <51760372+JasonHorkles@users.noreply.github.com>
2023-12-15 14:09:25 -0700

    Initial commit

---
+++ admin/view.blade.php
@@ -0,0 +1,15 @@
+<div class="row">
+  <div class="col-xs-3">
+
+    <!-- Overview -->
+    <div class="box">
+      <div class="box-header with-border">
+        <h3 class="box-title"><i class='bx bx-command' style='margin-right:5px;'></i>Overview</h3>
+      </div>
+      <div class="box-body">
+        <p>You are currently running version <code>^#version#^</code>.</p>
+      </div>
+    </div>
+  
+  </div>
+</div>
